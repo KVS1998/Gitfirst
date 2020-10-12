a8d6e540c31edc5a41de9181cc4857763d0168bf
KVSNIKHIL1998 <71979874+KVSNIKHIL1998@users.noreply.github.com>
2020-10-12 18:26:24 +0530

    Add files via upload

---
+++ avg.java
@@ -0,0 +1,16 @@
+package array;
+
+public class avg {
+
+	public static void main(String[] args) {
+		double arr[]= {10,20,30,40,50};
+		double total=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			total = total + arr[i];
+		}
+        double average = total/arr.length;
+        System.out.println("average is: " +average);
+	}
+
+}
